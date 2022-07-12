4c022e185c781d49ecbb6640bb26a97cc8da5824
Serhii Bortnik <serj3011@gmail.com>
2022-07-12 15:20:43 +0300

    user2 add some

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -4,5 +4,6 @@ public class Task1 {
     public static void main(String[] args) {
         System.out.println("Commit1");
         System.out.println("User2 add some changes");
+        System.out.println("User2 add some");
     }
 }
