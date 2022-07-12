f1615ef0e87f80f6edcd426453d646b4c464198f
Serhii Bortnik <serj3011@gmail.com>
2022-07-12 14:54:16 +0300

    user2 changed task1

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -3,5 +3,6 @@ package day0;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Commit1");
+        System.out.println("User2 add some changes");
     }
 }
